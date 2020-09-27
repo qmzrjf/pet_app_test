4185c9cc289db252b45a770f4d42c6b0e8a731cb
qmzrjf <qmzrjf12@gmail.com>
2020-09-27 18:17:43 +0300

    admin changes

--- src/pets_schedule/admin.py
+++ src/pets_schedule/admin.py
@@ -3,15 +3,26 @@ from pets_schedule.models import User, Pet, Schedule
 
 
 class UserAdmin(admin.ModelAdmin):
+    search_fields = ('username',)
     fields = ["email", "username", "avatar"]
+    list_display = ('username', 'email')
+    list_filter = ('username',)
+    ordering = ('-date_joined',)
 
 
 class PetAdmin(admin.ModelAdmin):
+    search_fields = ('name', 'kind', 'breed')
     fields = ["name", "date_of_birth", "gender", "kind", "breed", "avatar"]
+    list_display = ('name', 'gender', 'kind', 'breed', 'date_of_birth')
+    list_filter = ('name', 'gender', 'kind', 'breed')
+    ordering = ('-date_of_birth',)
 
 
 class ScheduleAdmin(admin.ModelAdmin):
+    search_fields = ('responsible_person', 'pet')
     fields = ["name", "pet", "responsible_person", "date_time"]
+    list_display = ('pet', 'responsible_person', 'name', 'date_time')
+    list_filter = ('pet', 'responsible_person')
 
 
 admin.site.register(User, UserAdmin)
